d4364ecd14b3369fb4317ad866072cb59ca974b0
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-11-12 22:01:47 +0900

    Update TCPServer.py

--- Assignment2/TCPServer.py
+++ Assignment2/TCPServer.py
@@ -5,7 +5,7 @@ serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 print ('The server is ready to receive')
-while True: #
+while True:
     connectionSocket, addr = serverSocket.accept()
     print("Connection has been established|"+"IP: "+addr[0]+"| serverPort: "+str(addr[1]))
     sentence = str(connectionSocket.recv(1024), "utf=8")
