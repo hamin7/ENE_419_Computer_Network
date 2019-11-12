e502bb12e71ca6a5c373ab925182f3d84f37e876
hamin7 <ggamini7@gmail.com>
2019-11-13 00:17:26 +0900

    Update TCPServer.py

--- Assignment2/TCPServer.py
+++ Assignment2/TCPServer.py
@@ -1,4 +1,17 @@
 from socket import *
+#import requests
+#from httpcache import CachingHTTPAdapter
+#from httpcache import HTTPcache
+
+#s = requests.Session()
+#s.mount('http://', CachingHTTPAdapter())
+
+#cache = httpcache(capacity=50)
+#cache.store(response)
+
+#cached_response = cache.retrieve(requests)
+
+#print(cached_response)
 
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
@@ -7,7 +20,7 @@ serverSocket.listen(1)
 print ('The server is ready to receive')
 while True:
     connectionSocket, addr = serverSocket.accept()
-    print("Connection has been established|"+"IP: "+addr[0]+"| serverPort: "+addr[1])
+    print("Connection has been established|"+"IP: "+addr[0]+"| serverPort: "+str(addr[1]))
     sentence = str(connectionSocket.recv(1024), "utf=8")
     capitalizedSentence = sentence.upper()
     print(capitalizedSentence, end="")
