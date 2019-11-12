876bf9fc1b6627f275acaa4fef7930c9b5a37b9d
hamin7 <ggamini7@gmail.com>
2019-11-12 23:59:47 +0900

    Update TCPServer.py

--- Assignment2/TCPServer.py
+++ Assignment2/TCPServer.py
@@ -7,7 +7,7 @@ serverSocket.listen(1)
 print ('The server is ready to receive')
 while True:
     connectionSocket, addr = serverSocket.accept()
-    print("Connection has been established|"+"IP: "+addr[0]+"| serverPort: "+str(addr[1]))
+    print("Connection has been established|"+"IP: "+addr[0]+"| serverPort: "+addr[1])
     sentence = str(connectionSocket.recv(1024), "utf=8")
     capitalizedSentence = sentence.upper()
     print(capitalizedSentence, end="")
