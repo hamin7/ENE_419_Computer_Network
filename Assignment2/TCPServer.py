f5e426e5f23353dd2e063c81d0b498c3ff95ab44
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-11-12 19:59:46 +0900

    update TCPServer.py

--- Assignment2/TCPServer.py
+++ Assignment2/TCPServer.py
@@ -1,14 +1,54 @@
 from socket import *
 
-serverPort = 12000
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(('',serverPort))
-serverSocket.listen(1)
+serverSocket = socket(AF_INET, SOCK_STREAM)
 
-print ("The server is ready to receive")
-while 1:
-    connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024)
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence)
+#소켓 생성
+def socket_create():
+    try:
+        global host
+        global serverPort
+        global serverSocket
+        host = ''
+        serverPort = 12000
+        serverSocket = socket()
+        serverSocket.connect(("127.0.0.1", serverPort))
+    except error as msg:
+        print("soket creation error: " + str(msg))
+
+#바인딩
+def socket_bind():
+    try:
+        global host
+        global serverPort
+        global serverSocket
+        print("Binding socket to serverPort: " + str(serverPort))
+        print("The server is ready to receive")
+        serverSocket.bind(('', serverPort))
+        #serverSocket.bind((host, serverPort))
+        serverSocket.listen(5)  # 왜 1이어야 함?
+    except error as msg:
+        print("Socket binding error: " + str(msg) + "\n" + "Retrying...")
+        socket_bind()
+
+#연결 수용
+def socket_accept():
+    connectionSocket, address = serverSocket.accept()
+    print("Connection has been established|"+"IP: "+address[0]+"| serverPort: "+str(address[1]))
+    send_commands(connectionSocket)
     connectionSocket.close()
+
+#명령
+def send_commands(connectionSocket):
+    while 1:
+        sentence = str(connectionSocket.recv(1024), "utf=8")     # 1024, 2048 무슨 의미.
+        capitalizedSentence = sentence.upper() 
+        print(capitalizedSentence, end="")
+        connectionSocket.send(capitalizedSentence.encode()) # encode 왜 하는거?
+        
+
+def main():
+    socket_create()
+    socket_bind()
+    socket_accept()
+
+main()
