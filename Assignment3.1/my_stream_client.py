c883d5ac2c43e921eefb53dd42cb1cd9104278c8
hamin7 <ggamini7@gmail.com>
2019-11-26 00:59:46 +0900

    Update my_stream_client.py

--- Assignment3.1/my_stream_client.py
+++ Assignment3.1/my_stream_client.py
@@ -1,12 +1,18 @@
 from websocket import create_connection
+import json
 
 ws = create_connection("wss://api.upbit.com/websocket/v1")
-print('Sending "[{"ticket":"UNIQUE_TICKET"},{"type":"trade","codes":["KRW- BTC"]},{"type":"orderbook","codes":["KRW-BTC"]}]"...')
+#print('Sending "[{"ticket":"UNIQUE_TICKET"},{"type":"trade","codes":["KRW- BTC"]},{"type":"orderbook","codes":["KRW-BTC"]}]"...')
 ws.send('[{"ticket":"UNIQUE_TICKET"},{"type":"trade","codes":["KRW- BTC"]},{"type":"orderbook","codes":["KRW-BTC"]}]')
 print("Sent")
 print("Receiving...")
 
 while(True):
+    ask_price = 0
     result =  ws.recv()
-    print("Received '%s'" % result)
+    json_data = json.loads(result)
+    orderbook = json_data["orderbook_units"][0]
+    #trade = json_data["trade_price"]
+    #print(orderbook)
+    print(json_data)
 ws.close()
