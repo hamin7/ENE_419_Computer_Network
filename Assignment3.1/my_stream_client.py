b96d87aec88fb73194eba3cfa3eb6a16a38d6dd1
hamin7 <ggamini7@gmail.com>
2019-11-26 22:56:45 +0900

    Update my_stream_client.py

--- Assignment3.1/my_stream_client.py
+++ Assignment3.1/my_stream_client.py
@@ -2,17 +2,29 @@ from websocket import create_connection
 import json
 
 ws = create_connection("wss://api.upbit.com/websocket/v1")
-#print('Sending "[{"ticket":"UNIQUE_TICKET"},{"type":"trade","codes":["KRW- BTC"]},{"type":"orderbook","codes":["KRW-BTC"]}]"...')
-ws.send('[{"ticket":"UNIQUE_TICKET"},{"type":"trade","codes":["KRW- BTC"]},{"type":"orderbook","codes":["KRW-BTC"]}]')
+
 print("Sent")
 print("Receiving...")
 
-while(True):
-    ask_price = 0
+while(True):    
+    ws.send('[{"ticket":"UNIQUE_TICKET"},{"type":"trade","codes":["KRW-BTC"]},{"type":"orderbook","codes":["KRW-ETH"]}]')
     result =  ws.recv()
     json_data = json.loads(result)
-    orderbook = json_data["orderbook_units"][0]
-    #trade = json_data["trade_price"]
-    #print(orderbook)
-    print(json_data)
+
+    if json_data["type"] == 'orderbook':
+        orderbook = json_data["orderbook_units"][0]
+        
+        ask_price = orderbook["ask_price"]
+        bid_price = orderbook["bid_price"]
+        ask_size = orderbook["ask_size"]
+        bid_size = orderbook["bid_size"]
+
+        print("ask_price : %s, bid_price : %s, ask_size : %s, bid_size : %s" %(ask_price,bid_price,ask_size,bid_size))
+
+    elif json_data["type"] == 'trade':
+        trade_price = json_data['trade_price']
+        trade_volume = json_data['trade_volume']
+        ask_bid = json_data['ask_bid']
+
+        print("trade_price : %s, trade_volume : %s, ask_bid : %s \n" %(trade_price,trade_volume,ask_bid))
 ws.close()
