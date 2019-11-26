3fb1006c94d6c455c9772658bce8e235e42fb152
hamin7 <ggamini7@gmail.com>
2019-11-26 23:01:11 +0900

    Update my_stream_client.py

--- Assignment3.1/my_stream_client.py
+++ Assignment3.1/my_stream_client.py
@@ -19,12 +19,12 @@ while(True):
         ask_size = orderbook["ask_size"]
         bid_size = orderbook["bid_size"]
 
-        print("ask_price : %s, bid_price : %s, ask_size : %s, bid_size : %s" %(ask_price,bid_price,ask_size,bid_size))
+        print("ask_price : %s, bid_price : %s, ask_size : %s, bid_size : %s \n" %(ask_price,bid_price,ask_size,bid_size))
 
     elif json_data["type"] == 'trade':
         trade_price = json_data['trade_price']
         trade_volume = json_data['trade_volume']
         ask_bid = json_data['ask_bid']
 
-        print("trade_price : %s, trade_volume : %s, ask_bid : %s \n" %(trade_price,trade_volume,ask_bid))
+        print("trade_price : %s, trade_volume : %s, ask_bid : %s" %(trade_price,trade_volume,ask_bid))
 ws.close()
